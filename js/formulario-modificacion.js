4a9c5866ef53f0d233531edf76dc606d19f6b8a9
ExequielMuñoz <79278576+Exequie-l44@users.noreply.github.com>
2024-01-05 14:40:38 -0300

    cambios en las tablas

--- js/formulario-modificacion.js
+++ js/formulario-modificacion.js
@@ -1,8 +1,6 @@
-import '../css/style.css'
-import {insertarFilaEnTabla} from './tabla-modificacion'
 
 
-export  const form = document.getElementById("transaccionFormulario");
+ let form = document.getElementById("transaccionFormulario");
 
 form.addEventListener( "submit", function(event) {
 
@@ -13,5 +11,40 @@ form.addEventListener( "submit", function(event) {
 
 
 })
+
+export function insertarFilaEnTabla(transaccionFormData){
+  
+
+    let nuevaTransanccionRowRef =  document.getElementById("tablaTransaccion").insertRow(-1); //Agrega una fila al final de la tabla con el -1
+  
+    let nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(0); //Agrega un nueva celda a la tabla 
+    //textContent remplaza en el html por el transaccionFormData.get que trae el name del formulario
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(1);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("fecha")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(2);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("cliente")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(3);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("hora")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(4);
+    nuevoTipoCeldaRef.textContent =new FormData(form).get("colaborador")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(5);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("cajero")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(6);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("metodo")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(7);
+    nuevoTipoCeldaRef.textContent =new FormData(form).get("descuento")
+    
+    nuevoTipoCeldaRef = nuevaTransanccionRowRef.insertCell(8);
+    nuevoTipoCeldaRef.textContent = new FormData(form).get("pagoTotal")
+  }
+  
 
 
